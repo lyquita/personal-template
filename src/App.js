ec552d0cc1c33b312dac352438f087ac702407ef
hireoochen@gmail.com <hireoochen@gmail.com>
2022-11-16 23:37:16 +0800

    set up tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h2>Welcome to React App</h2>
+      <h2 className='underline'>Welcome to React App</h2>
       <h3>Date : {new Date().toDateString()}</h3>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,47 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const LinkTypePlugin =
+  require('html-webpack-link-type-plugin').HtmlWebpackLinkTypePlugin;
 
-new CleanWebpackPlugin()
+new CleanWebpackPlugin();
 
 module.exports = {
   // Where files should be sent once they are bundled
- output: {
-   path: path.join(__dirname, '/dist'),
-   filename: 'index.bundle.js'
- },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
+  },
   // webpack 5 comes with devServer which loads in development mode
- devServer: {
-   port: 3000,
-   static: './dist',
-   hot: true,
-   compress: true,
- },
-  // Rules of how webpack will take our files, complie & bundle them for the browser 
- module: {
-   rules: [
-     {
-       test: /\.(js|jsx)$/,
-       exclude: /nodeModules/,
-       use: {
-         loader: 'babel-loader'
-       }
-     },
-     {
-       test: /\.css$/,
-       use: ['style-loader', 'css-loader']
-     }
-   ]
- },
- devtool: 'inline-source-map',
- plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
-}
-
+  devServer: {
+    port: 3000,
+    static: path.join(__dirname, 'dist'),
+    hot: true,
+    compress: true,
+  },
+  // Rules of how webpack will take our files, complie & bundle them for the browser
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(gif|svg|jpg|png)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new LinkTypePlugin({'*.css' : 'text/css'}),
+  ],
+};
